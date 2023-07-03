4b5f50f15304c71ba01109f5b6329f395ab619e8
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2023-07-03 18:25:02 -0500

    feat: Update 2

--- c_yacc.py
+++ c_yacc.py
@@ -4,6 +4,68 @@ import c_lex
 tokens = c_lex.tokens
 
 
+def p_program(t):
+    """
+    program : program function_declaration
+            | program variable_declaration
+            | empty
+    """
+    t[0] = t[1]
+    pass
+
+
+def p_empty(t):
+    "empty :"
+    pass
+
+
+def p_function_declaration(t):
+    """
+    function_declaration : type ID L_PAREN parameter_list R_PAREN L_BRACE statement R_BRACE
+    """
+    pass
+
+
+def p_parameter_list(t):
+    """
+    parameter_list : paramater
+                   | parameter_list COMMA parameter
+                   | empty
+    """
+    pass
+
+
+def p_parameter(t):
+    """
+    parameter : type ID
+              | type
+    """
+    pass
+
+
+def p_variable_declaration(t):
+    """
+    variable_declaration : type declaring_variables SEMICOLON
+    """
+    pass
+
+
+def p_declaring_variables(t):
+    """
+    declaring_variables : declaring_variable
+                        | declaring_variables COMMA declaring_variable
+    """
+    pass
+
+
+def p_declaring_variable(t):
+    """
+    declaring_variable : ID
+                       | ID EQUALS expression
+    """
+    pass
+
+
 def p_conditional(t):
     """
     s_conditional : s_if
@@ -26,36 +88,10 @@ def p_s_if_else(t):
     pass
 
 
-def p_program(t):
-    t[0] = t[1]
-
-
 def p_type(t):
     """
-    type_specifier : VOID
-                   | INT
-    """
-    pass
-
-
-def p_variable_declaration(t):
-    """
-    variable_declaration : p_type declaring_variables SEMICOLON
-    """
-    pass
-
-
-def p_declaring_variables(t):
-    """
-    declaring_variables : declaring_variable
-                        | declaring_variables COMMA declaring_variable
-    """
-
-
-def p_declaring_variable(t):
-    """
-    declaring_variable : ID
-                       | ID EQUALS expression
+    type : VOID
+         | INT
     """
     pass
 
@@ -63,13 +99,15 @@ def p_declaring_variable(t):
 def p_expression(t):
     """
     expression : int_literal
+               | L_PAREN expression R_PAREN
     """
     pass
 
 
 def p_statement(t):
     """
-    statement : int_literal
+    statement : expression SEMICOLON
+              | p_s_assign
     """
     pass
 
@@ -81,55 +119,103 @@ def p_int_literal(t):
     pass
 
 
-def p_statement_assign(t, s):
-    "statement : NAME EQUALS expression SEMICOLON"
+def p_s_assign(t):
+    """
+    s_assign : ID EQUALS expression SEMICOLON
+    """
     pass
 
 
-def p_expression_plus(p):
-    "expression : expression PLUS term"
+def p_e_plus(t):
+    """
+    e_plus : expression PLUS expression
+    """
     pass
 
 
-def p_expression_plus(p):
-    "expression : expression PLUS term"
-
-
-def p_expression_minus(p):
-    "expression : expression MINUS term"
+def p_e_minus(t):
+    """
+    e_minus : expression MINUS expression
+    """
     pass
 
 
-def p_expression_term(p):
-    "expression : term"
-
-
-def p_term_times(p):
-    "term : term TIMES factor"
-
-
-def p_term_div(p):
-    "term : term DIVIDE factor"
+def p_e_times(t):
+    """
+    e_times : expression TIMES expression
+    """
     pass
 
 
-def p_term_factor(p):
-    "term : factor"
+def p_e_divide(t):
+    """
+    e_divide : expression DIVIDE expression
+    """
     pass
 
 
-def p_factor_num(p):
-    "factor : NUMBER"
+def p_e_mod(t):
+    "e_mod : expression MOD expression"
     pass
 
 
-def p_factor_expr(p):
-    "factor : LPAREN expression RPAREN"
+def p_e_lor(t):
+    "e_lor : expression LOR expression"
     pass
 
 
-def p_error(p):
-    print("Syntax error in input!")
+def p_e_land(t):
+    "e_land : expression LAND expression"
+    pass
+
+
+def p_e_lnot(t):
+    """
+    e_lnot : LNOT expression
+    """
+    pass
+
+
+def p_e_lt(t):
+    """
+    e_lnot : expression LT expression
+    """
+    pass
+
+
+def p_e_gt(t):
+    """
+    e_gt : expression GT expression
+    """
+    pass
+
+
+def p_e_le(t):
+    """
+    e_le : expression LE expression
+    """
+    pass
+
+
+def p_e_ge(t):
+    """
+    e_ge : expression GE expression
+    """
+    pass
+
+
+def p_e_eq(t):
+    """
+    e_eq : expression EQ expression
+    """
+    pass
+
+
+def p_e_ne(t):
+    """
+    e_ne : expression NE expression
+    """
+    pass
 
 
 def p_puts(t):
