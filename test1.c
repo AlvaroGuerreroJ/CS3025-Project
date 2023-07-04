510c02a11c31c38b32aeb4f02e42a8ac7a67129e
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2023-07-04 12:48:01 -0500

    test: Add some code more

--- test1.c
+++ test1.c
@@ -1,15 +1,21 @@
 int x, y;
 
-main() {
+int main(int a, int b) {
   x = 6;
   y = 1;
+
   while (x > 0) {
     y = y * x;
     x = x - 1;
   }
+
   puts("El factorial de 6 es : ");
   putw(y);
   puts("\n");
   puts("El valor de x+1 es : ");
   putw(x + 1);
+
+  int v0;
+  int v1 = ((2 < 1) == (1 < 2)) || 1;
+  int v2 = 3 - 2 * 9 + 11;
 }
