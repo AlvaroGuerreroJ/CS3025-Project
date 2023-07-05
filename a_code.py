e89d9be3089d0b38c12772074c2526370f8319a6
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2023-07-05 11:30:09 -0500

    feat: Code generation

--- a_code.py
+++ a_code.py
@@ -1,13 +1,28 @@
 import graphviz
+from collections import defaultdict
+import io
 
-label_n = 1
 
+class CodeGen:
+    __slots__ = ("out", "label_n", "temp_n")
 
-def gen_label():
-    global label_n
-    label = f":G{label_n}"
-    label_n += 1
-    return label
+    def __init__(self):
+        self.out = io.StringIO()
+        self.label_n = defaultdict(int)
+        self.temp_n = 1
+
+    def gen_label(self, prefix):
+        label = f":{prefix}_{self.label_n[prefix]}"
+        self.label_n[prefix] += 1
+        return label
+
+    def gen_temp(self):
+        temp = f"t{self.temp_n}"
+        self.temp_n += 1
+        return temp
+
+    def write(self, string):
+        self.out.write(string + "\n")
 
 
 class DotHelper:
@@ -45,6 +60,11 @@ class Node:
         pass
 
 
+class Expression(Node):
+    def gen_code(self, codegen: CodeGen):
+        self.rvalue(codegen)
+
+
 class NSBlock(Node):
     __slots__ = "statements"
 
@@ -59,6 +79,10 @@ class NSBlock(Node):
             dih.create_edge(id_, on)
 
         return id_
+
+    def gen_code(self, codegen: CodeGen):
+        for st in self.statements:
+            st.gen_code(codegen)
 
 
 class Block(Node):
@@ -76,8 +100,12 @@ class Block(Node):
 
         return id_
 
+    def gen_code(self, codegen: CodeGen):
+        for st in self.statements:
+            st.gen_code(codegen)
 
-class Id(Node):
+
+class Id(Expression):
     __slots__ = "var_name"
 
     def __init__(self, var_name):
@@ -89,6 +117,12 @@ class Id(Node):
         dih.create_edge(id_, var_id)
 
         return id_
+
+    def lvalue(self, codegen: CodeGen):
+        return self.var_name
+
+    def rvalue(self, codegen: CodeGen):
+        return self.var_name
 
 
 class FId(Node):
@@ -130,6 +164,18 @@ class FunctionDefinition(Node):
 
         return id_
 
+    def gen_code(self, codegen: CodeGen):
+        l_f = f":function_{self.fname.var_name}"
+
+        l_f_end = f"{l_f}_end"
+        codegen.write(f"goto {l_f_end}")
+
+        codegen.write(l_f)
+        print(self.parameters)
+        self.body.gen_code(codegen)
+        codegen.write("return")
+        codegen.write(l_f_end)
+
 
 class VariableDeclaration(Node):
     __slots__ = ("var_name", "v_type")
@@ -139,7 +185,7 @@ class VariableDeclaration(Node):
         self.v_type = v_type
 
     def draw(self, dih: DotHelper):
-        id_ = dih.create_node(f"VariableDeclaration")
+        id_ = dih.create_node("VariableDeclaration")
 
         var_name_id = self.var_name.draw(dih)
         dih.create_edge(id_, var_name_id)
@@ -148,6 +194,9 @@ class VariableDeclaration(Node):
         dih.create_edge(id_, type_id, "type")
 
         return id_
+
+    def gen_code(self, codegen: CodeGen):
+        pass
 
 
 class If(Node):
@@ -167,6 +216,16 @@ class If(Node):
         dih.create_edge(id_, then_id, "then")
 
         return id_
+
+    def gen_code(self, codegen: CodeGen):
+        l_if = codegen.gen_label("if")
+        l_if_skip = f"{l_if}_skip"
+
+        test_rv = self.condition.rvalue(codegen)
+
+        codegen.write(f"ifFalse {test_rv} goto {l_if_skip}")
+        self.then_statement.gen_code(codegen)
+        codegen.write(l_if_skip)
 
 
 class IfElse(Node):
@@ -191,6 +250,22 @@ class IfElse(Node):
 
         return id_
 
+    def gen_code(self, codegen: CodeGen):
+        l_if = codegen.gen_label("if")
+        l_if_else = f"{l_if}_else"
+        l_if_end = f"{l_if}_end"
+
+        test_rv = self.condition.rvalue(codegen)
+
+        codegen.write(f"ifFalse {test_rv} goto {l_if_else}")
+        self.then_statement.gen_code(codegen)
+        codegen.write(f"goto {l_if_end}")
+
+        codegen.write(l_if_else)
+        self.else_statement.gen_code(codegen)
+
+        codegen.write(l_if_end)
+
 
 class While(Node):
     __slots__ = ("condition", "body")
@@ -209,6 +284,20 @@ class While(Node):
         dih.create_edge(id_, body_id, "body")
 
         return id_
+
+    def gen_code(self, codegen: CodeGen):
+        l_while = codegen.gen_label("while")
+        l_while_start = f"{l_while}_start"
+        l_while_end = f"{l_while}_end"
+
+        codegen.write(l_while_start)
+
+        test_rv = self.condition.rvalue(codegen)
+        codegen.write(f"ifFalse {test_rv} goto {l_while_end}")
+        self.body.gen_code(codegen)
+        codegen.write(f"goto {l_while_start}")
+
+        codegen.write(l_while_end)
 
 
 class For(Node):
@@ -238,7 +327,7 @@ class For(Node):
         return id_
 
 
-class IntLiteral(Node):
+class IntLiteral(Expression):
     __slots__ = "value"
 
     def __init__(self, value):
@@ -249,8 +338,11 @@ class IntLiteral(Node):
 
         return id_
 
+    def rvalue(self, codegen: CodeGen):
+        return self.value
 
-class Assignment(Node):
+
+class Assignment(Expression):
     __slots__ = ("id_", "exp")
 
     def __init__(self, id_, exp):
@@ -268,8 +360,35 @@ class Assignment(Node):
 
         return id_
 
+    def rvalue(self, codegen: CodeGen):
+        exp_rv = self.exp.rvalue(codegen)
+        id_lv = self.id_.lvalue(codegen)
+        codegen.write(f"{id_lv} = {exp_rv}")
+        return self.id_
 
-class BinaryExp(Node):
+
+class LNot:
+    __slots__ = ("exp",)
+
+    def __init__(self, exp):
+        self.exp = exp
+
+    def rvalue(self, codegen: CodeGen):
+        exp_rv = self.exp.rvalue(codegen)
+        tv = codegen.gen_temp()
+        codegen.write(f"{tv} = not {exp_rv}")
+
+        return tv
+
+    def draw(self, dih: DotHelper):
+        id_ = dih.create_node("Not")
+        exp_id = self.exp.draw(dih)
+        dih.create_edge(id_, exp_id)
+
+        return id_
+
+
+class BinaryExp(Expression):
     __slots__ = ("exp1", "exp2")
 
     def __init__(self, exp1, exp2):
@@ -287,61 +406,80 @@ class BinaryExp(Node):
 
         return id_
 
+    def rvalue(self, codegen: CodeGen):
+        exp1_rv = self.exp1.rvalue(codegen)
+        exp2_rv = self.exp2.rvalue(codegen)
+
+        tv = codegen.gen_temp()
+
+        codegen.write(f"{tv} = {exp1_rv} {self.op} {exp2_rv}")
+
+        return tv
+
 
 class Plus(BinaryExp):
     c_op_name = "Plus"
+    op = "+"
 
 
 class Minus(BinaryExp):
     c_op_name = "Minus"
+    op = "-"
 
 
 class Times(BinaryExp):
     c_op_name = "Times"
+    op = "*"
 
 
 class Divide(BinaryExp):
     c_op_name = "Divide"
+    op = "/"
 
 
 class Mod(BinaryExp):
     c_op_name = "Mod"
+    op = "mod"
 
 
 class LOr(BinaryExp):
     c_op_name = "LOr"
+    op = "||"
 
 
 class LAnd(BinaryExp):
     c_op_name = "LAnd"
-
-
-class LNot(BinaryExp):
-    c_op_name = "LNot"
+    op = "&&"
 
 
 class LT(BinaryExp):
     c_op_name = "LT"
+    op = "<"
 
 
 class GT(BinaryExp):
     c_op_name = "GT"
+    op = ">"
 
 
 class LE(BinaryExp):
     c_op_name = "LE"
+    op = "<="
 
 
 class GE(BinaryExp):
     c_op_name = "GE"
+    op = ">="
 
 
 class EQ(BinaryExp):
     c_op_name = "EQ"
+    op = "=="
 
 
 class NE(BinaryExp):
     c_op_name = "NE"
+    op = "!="
 
 
 class Puts(Node):
@@ -358,6 +496,9 @@ class Puts(Node):
 
         return id_
 
+    def gen_code(self, codegen: CodeGen):
+        codegen.write(f"puts {repr(self.string)}")
+
 
 class Putw(Node):
     __slots__ = "exp"
@@ -372,3 +513,7 @@ class Putw(Node):
         dih.create_edge(id_, exp_id)
 
         return id_
+
+    def gen_code(self, codegen: CodeGen):
+        exp_rv = self.exp.rvalue(codegen)
+        codegen.write(f"putw {exp_rv}")
