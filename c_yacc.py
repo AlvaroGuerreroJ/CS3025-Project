0bc757a0190cc7fb1b47405471964fd7d458d3dc
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2023-07-05 07:06:35 -0500

    feat(yacc): Construct AST using classes

--- c_yacc.py
+++ c_yacc.py
@@ -1,5 +1,6 @@
-import ply.yacc as yacc
+import a_code as c
 import c_lex
+import ply.yacc as yacc
 
 tokens = c_lex.tokens
 
@@ -23,9 +24,9 @@ def p_program(t):
             | empty
     """
     if len(t) == 3:
-        t[0] = ("program",) + t[1][1:] + (t[2],)
+        t[0] = c.Block(t[1].statements + [t[2]])
     else:
-        t[0] = ("program",)
+        t[0] = c.Block([])
 
 
 def p_empty(t):
@@ -39,14 +40,21 @@ def p_id(t):
     """
     id : ID
     """
-    t[0] = ("identifier", t[1])
+    t[0] = c.Id(t[1])
+
+
+def p_fid(t):
+    """
+    fid : ID
+    """
+    t[0] = c.FId(t[1])
 
 
 def p_function_definition(t):
     """
-    function_definition : type id L_PAREN parameter_list R_PAREN statement_block
+    function_definition : type fid L_PAREN parameter_list R_PAREN statement_block
     """
-    t[0] = ("function_definition", t[1], t[2], t[4], t[6])
+    t[0] = c.FunctionDefinition(t[2], t[1], t[4], t[6])
 
 
 def p_parameter_list(t):
@@ -55,9 +63,9 @@ def p_parameter_list(t):
                    | parameter parameter_list_others
     """
     if len(t) == 3:
-        t[0] = ("parameter_list", t[1]) + t[2][1:]
+        t[0] = [t[1]] + t[2]
     else:
-        t[0] = ("parameter_list",)
+        t[0] = []
 
 
 def p_parameter_list_others(t):
@@ -66,9 +74,9 @@ def p_parameter_list_others(t):
                           | empty
     """
     if len(t) == 2:
-        t[0] = ("parameter_list_others",)
+        t[0] = []
     else:
-        t[0] = ("parameter_list_others",) + t[1][1:] + (t[3],)
+        t[0] = t[1] + [t[3]]
 
 
 def p_parameter(t):
@@ -77,16 +85,24 @@ def p_parameter(t):
               | type
     """
     if len(t) == 3:
-        t[0] = ("parameter", t[1], t[2])
+        t[0] = (t[1], t[2])
     else:
-        t[0] = ("parameter", t[1])
+        t[0] = (t[1],)
 
 
 def p_variable_declaration(t):
     """
     variable_declaration : type declaring_variables SEMICOLON
     """
-    t[0] = ("variable_declaration", t[1], t[2])
+    ret = []
+    for tup in t[2]:
+        if len(tup) == 2:
+            ret.append(c.VariableDeclaration(tup[0], t[1]))
+            ret.append(c.Assignment(tup[0], tup[1]))
+        else:
+            ret.append(c.VariableDeclaration(tup[0], t[1]))
+
+    t[0] = c.NSBlock(ret)
 
 
 def p_declaring_variables(t):
@@ -95,9 +111,9 @@ def p_declaring_variables(t):
                         | declaring_variables COMMA declaring_variable
     """
     if len(t) == 2:
-        t[0] = ("declaring_variables", t[1])
+        t[0] = [t[1]]
     else:
-        t[0] = ("declaring_variables",) + t[1][1:] + (t[3],)
+        t[0] = t[1] + [t[3]]
 
 
 def p_declaring_variable(t):
@@ -106,9 +122,9 @@ def p_declaring_variable(t):
                        | id EQUALS expression
     """
     if len(t) == 2:
-        t[0] = ("declaring_variable", t[1])
+        t[0] = (t[1],)
     else:
-        t[0] = ("declaring_variable", t[1], t[3])
+        t[0] = (t[1], t[3])
 
 
 def p_s_conditional(t):
@@ -116,21 +132,21 @@ def p_s_conditional(t):
     s_conditional : s_if
                   | s_if_else
     """
-    t[0] = ("conditional", t[1])
+    t[0] = t[1]
 
 
 def p_s_if(t):
     """
     s_if : IF L_PAREN expression R_PAREN statement
     """
-    t[0] = ("if", t[3], t[5])
+    t[0] = c.If(t[3], t[5])
 
 
 def p_s_if_else(t):
     """
     s_if_else : IF L_PAREN expression R_PAREN statement ELSE statement
     """
-    t[0] = ("if_else", t[3], t[5], t[7])
+    t[0] = c.IfElse(t[3], t[5], t[7])
 
 
 def p_type(t):
@@ -138,7 +154,7 @@ def p_type(t):
     type : VOID
          | INT
     """
-    t[0] = ("type", t[1])
+    t[0] = t[1]
 
 
 def p_expression(t):
@@ -172,7 +188,7 @@ def p_statement_block(t):
     """
     statement_block : L_BRACE statements R_BRACE
     """
-    t[0] = t[2]
+    t[0] = c.Block(t[2])
 
 
 def p_statements(t):
@@ -181,9 +197,9 @@ def p_statements(t):
                | empty
     """
     if len(t) == 2:
-        t[0] = ("statements",)
+        t[0] = []
     else:
-        t[0] = ("statements",) + t[1][1:] + (t[2],)
+        t[0] = t[1] + [t[2]]
 
 
 def p_statement(t):
@@ -203,113 +219,113 @@ def p_s_while(t):
     """
     s_while : WHILE L_PAREN expression R_PAREN statement
     """
-    t[0] = ("while", t[3], t[5])
+    t[0] = c.While(t[3], t[5])
 
 
 def p_int_literal(t):
     """
     int_literal : INT_LITERAL
     """
-    t[0] = ("int_literal", int(t[1]))
+    t[0] = c.IntLiteral(int(t[1]))
 
 
 def p_e_assign(t):
     """
     e_assign : id EQUALS expression
     """
-    t[0] = ("assigns", t[1], t[3])
+    t[0] = c.Assignment(t[1], t[3])
 
 
 def p_e_plus(t):
     """
     e_plus : expression PLUS expression
     """
-    t[0] = ("plus", t[1], t[3])
+    t[0] = c.Plus(t[1], t[3])
 
 
 def p_e_minus(t):
     """
     e_minus : expression MINUS expression
     """
-    t[0] = ("minus", t[1], t[3])
+    t[0] = c.Minus(t[1], t[3])
 
 
 def p_e_times(t):
     """
     e_times : expression TIMES expression
     """
-    t[0] = ("times", t[1], t[3])
+    t[0] = c.Times(t[1], t[3])
 
 
 def p_e_divide(t):
     """
     e_divide : expression DIVIDE expression
     """
-    t[0] = ("divide", t[1], t[3])
+    t[0] = c.Divide(t[1], t[3])
 
 
 def p_e_mod(t):
     "e_mod : expression MOD expression"
-    t[0] = ("mod", t[1], t[3])
+    t[0] = c.Mod(t[1], t[3])
 
 
 def p_e_lor(t):
     "e_lor : expression LOR expression"
-    t[0] = ("lor", t[1], t[3])
+    t[0] = c.LOr(t[1], t[3])
 
 
 def p_e_land(t):
     "e_land : expression LAND expression"
-    t[0] = ("land", t[1], t[3])
+    t[0] = c.LAnd(t[1], t[3])
 
 
 def p_e_lnot(t):
     """
     e_lnot : LNOT expression
     """
-    t[0] = ("lnot", t[1], t[3])
+    t[0] = c.LNot(t[1])
 
 
 def p_e_lt(t):
     """
     e_lt : expression LT expression
     """
-    t[0] = ("lt", t[1], t[3])
+    t[0] = c.LT(t[1], t[3])
 
 
 def p_e_gt(t):
     """
     e_gt : expression GT expression
     """
-    t[0] = ("gt", t[1], t[3])
+    t[0] = c.GT(t[1], t[3])
 
 
 def p_e_le(t):
     """
     e_le : expression LE expression
     """
-    t[0] = ("le", t[1], t[3])
+    t[0] = c.LE(t[1], t[3])
 
 
 def p_e_ge(t):
     """
     e_ge : expression GE expression
     """
-    t[0] = ("ge", t[1], t[3])
+    t[0] = c.GE(t[1], t[3])
 
 
 def p_e_eq(t):
     """
     e_eq : expression EQ expression
     """
-    t[0] = ("eq", t[1], t[3])
+    t[0] = c.EQ(t[1], t[3])
 
 
 def p_e_ne(t):
     """
     e_ne : expression NE expression
     """
-    t[0] = ("ne", t[1], t[3])
+    t[0] = c.NE(t[1], t[3])
 
 
 def p_string_literal(t):
@@ -317,21 +333,21 @@ def p_string_literal(t):
     string_literal : STRING_LITERAL
     """
     s = t[1][1:-1]
-    t[0] = ("string_literal", s)
+    t[0] = s
 
 
 def p_s_puts(t):
     """
     s_puts : PUTS L_PAREN string_literal R_PAREN SEMICOLON
     """
-    t[0] = ("puts", t[3])
+    t[0] = c.Puts(t[3])
 
 
 def p_s_putw(t):
     """
     s_putw : PUTW L_PAREN expression R_PAREN SEMICOLON
     """
-    t[0] = ("putw", t[3])
+    t[0] = c.Putw(t[3])
 
 
 parser = yacc.yacc(debug=True)
