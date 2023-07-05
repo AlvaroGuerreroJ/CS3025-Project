fa18e33891a20c163a0ed8425ecc179a5a220324
Alvaro Guerrero Jiménez <alvaro.guerrero@utec.edu.pe>
2023-07-05 11:34:51 -0500

    test(example): Add some more

--- test1.c
+++ test1.c
@@ -1,8 +1,11 @@
 int x, y;
 
+int z = 99, f;
+
 int main(int a, int b) {
   x = 6;
   y = 1;
+  z = x + y;
 
   while (x > 0) {
     y = y * x;
@@ -11,11 +14,18 @@ int main(int a, int b) {
 
   puts("El factorial de 6 es : ");
   putw(y);
-  puts("\n");
+  puts("...\n");
   puts("El valor de x+1 es : ");
   putw(x + 1);
+
+  if (a == 99 + 1 || b == 10 - 1) {
+    puts("Hello");
+  } else {
+    puts("Bye");
+  }
 
   int v0;
   int v1 = ((2 < 1) == (1 < 2)) || 1;
   int v2 = 3 - 2 * 9 + 11;
+  int v3 = !v0;
 }
